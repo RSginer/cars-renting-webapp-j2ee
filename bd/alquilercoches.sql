7b51f7afe05a14cf117f22f4dfe818fb180b2a28
rsginer <r.solerginer@gmail.com>
2017-04-23 21:29:33 +0200

    base de datos y diagrama

--- bd/alquilercoches.sql
+++ bd/alquilercoches.sql
@@ -1,22 +1,30 @@
--- --------------------------------------------------------
--- Host:                         localhost
--- Versión del servidor:         10.1.16-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win32
--- HeidiSQL Versión:             9.3.0.4984
--- --------------------------------------------------------
+CREATE DATABASE  IF NOT EXISTS `alquilercoches` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `alquilercoches`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: alquilercoches
+-- ------------------------------------------------------
+-- Server version	5.5.24-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Volcando estructura de base de datos para alquilercoches
-CREATE DATABASE IF NOT EXISTS `alquilercoches` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `alquilercoches`;
+--
+-- Table structure for table `bancos`
+--
 
-
--- Volcando estructura para tabla alquilercoches.bancos
-CREATE TABLE IF NOT EXISTS `bancos` (
+DROP TABLE IF EXISTS `bancos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bancos` (
   `id_banco` int(11) NOT NULL AUTO_INCREMENT,
   `direccion` int(11) NOT NULL DEFAULT '0',
   `poblacion` int(11) NOT NULL DEFAULT '0',
@@ -24,28 +32,49 @@ CREATE TABLE IF NOT EXISTS `bancos` (
   `nombre` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_banco`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Volcando datos para la tabla alquilercoches.bancos: ~0 rows (aproximadamente)
-DELETE FROM `bancos`;
+--
+-- Dumping data for table `bancos`
+--
+
+LOCK TABLES `bancos` WRITE;
 /*!40000 ALTER TABLE `bancos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `bancos` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `categorias`
+--
 
--- Volcando estructura para tabla alquilercoches.categorias
-CREATE TABLE IF NOT EXISTS `categorias` (
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorias` (
   `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(50) DEFAULT '0',
   PRIMARY KEY (`id_categoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Volcando datos para la tabla alquilercoches.categorias: ~0 rows (aproximadamente)
-DELETE FROM `categorias`;
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
 /*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'Coche deportivo');
 /*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `clientes`
+--
 
--- Volcando estructura para tabla alquilercoches.clientes
-CREATE TABLE IF NOT EXISTS `clientes` (
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
   `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL DEFAULT '0',
   `apellidos` varchar(50) NOT NULL DEFAULT '0',
@@ -54,69 +83,134 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   `provincia` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Volcando datos para la tabla alquilercoches.clientes: ~0 rows (aproximadamente)
-DELETE FROM `clientes`;
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `clientes_vehiculos`
+--
 
--- Volcando estructura para tabla alquilercoches.clientes_vehiculos
-CREATE TABLE IF NOT EXISTS `clientes_vehiculos` (
+DROP TABLE IF EXISTS `clientes_vehiculos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes_vehiculos` (
   `cliente` int(11) NOT NULL,
   `vehiculo` int(11) NOT NULL,
-  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `fecha_alquiler` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `precio` float NOT NULL,
-  PRIMARY KEY (`cliente`,`vehiculo`,`fecha`),
+  `feche_entrega` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`cliente`,`vehiculo`,`fecha_alquiler`),
   KEY `FK_vehiculo` (`vehiculo`),
   CONSTRAINT `FK_cliente` FOREIGN KEY (`cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_vehiculo` FOREIGN KEY (`vehiculo`) REFERENCES `vehiculos` (`id_vehiculo`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Volcando datos para la tabla alquilercoches.clientes_vehiculos: ~0 rows (aproximadamente)
-DELETE FROM `clientes_vehiculos`;
+--
+-- Dumping data for table `clientes_vehiculos`
+--
+
+LOCK TABLES `clientes_vehiculos` WRITE;
 /*!40000 ALTER TABLE `clientes_vehiculos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `clientes_vehiculos` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `ficha_tecnica`
+--
 
--- Volcando estructura para tabla alquilercoches.ficha_tecnica
-CREATE TABLE IF NOT EXISTS `ficha_tecnica` (
+DROP TABLE IF EXISTS `ficha_tecnica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ficha_tecnica` (
   `id_ficha_tecnica` int(11) NOT NULL AUTO_INCREMENT,
   `propietario` varchar(50) NOT NULL,
   `fecha_itv` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `observaciones` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_ficha_tecnica`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Volcando datos para la tabla alquilercoches.ficha_tecnica: ~0 rows (aproximadamente)
-DELETE FROM `ficha_tecnica`;
+--
+-- Dumping data for table `ficha_tecnica`
+--
+
+LOCK TABLES `ficha_tecnica` WRITE;
 /*!40000 ALTER TABLE `ficha_tecnica` DISABLE KEYS */;
 /*!40000 ALTER TABLE `ficha_tecnica` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `sequence`
+--
 
--- Volcando estructura para tabla alquilercoches.tarjetas
-CREATE TABLE IF NOT EXISTS `tarjetas` (
+DROP TABLE IF EXISTS `sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sequence` (
+  `SEQ_NAME` varchar(50) NOT NULL,
+  `SEQ_COUNT` decimal(38,0) DEFAULT NULL,
+  PRIMARY KEY (`SEQ_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sequence`
+--
+
+LOCK TABLES `sequence` WRITE;
+/*!40000 ALTER TABLE `sequence` DISABLE KEYS */;
+INSERT INTO `sequence` VALUES ('SEQ_GEN',0);
+/*!40000 ALTER TABLE `sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tarjetas`
+--
+
+DROP TABLE IF EXISTS `tarjetas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tarjetas` (
   `id_tarjeta` int(11) NOT NULL AUTO_INCREMENT,
   `numero` varchar(50) NOT NULL DEFAULT '0',
   `limite` int(11) NOT NULL DEFAULT '0',
   `idx` int(11) NOT NULL DEFAULT '0',
-  `cliente` int(11),
-  `banco` int(11),
+  `cliente` int(11) DEFAULT NULL,
+  `banco` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_tarjeta`),
   KEY `FK_tarjeta_cliente` (`cliente`),
   KEY `FK_tarjeta_banco` (`banco`),
   CONSTRAINT `FK_tarjeta_banco` FOREIGN KEY (`banco`) REFERENCES `bancos` (`id_banco`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_tarjeta_cliente` FOREIGN KEY (`cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Volcando datos para la tabla alquilercoches.tarjetas: ~0 rows (aproximadamente)
-DELETE FROM `tarjetas`;
+--
+-- Dumping data for table `tarjetas`
+--
+
+LOCK TABLES `tarjetas` WRITE;
 /*!40000 ALTER TABLE `tarjetas` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tarjetas` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `vehiculos`
+--
 
--- Volcando estructura para tabla alquilercoches.vehiculos
-CREATE TABLE IF NOT EXISTS `vehiculos` (
+DROP TABLE IF EXISTS `vehiculos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vehiculos` (
   `id_vehiculo` int(11) NOT NULL AUTO_INCREMENT,
   `bastidor` varchar(50) NOT NULL DEFAULT '0',
   `matricula` varchar(50) NOT NULL DEFAULT '0',
@@ -124,18 +218,31 @@ CREATE TABLE IF NOT EXISTS `vehiculos` (
   `kilometros` varchar(50) DEFAULT '0',
   `modelo` varchar(50) NOT NULL DEFAULT '0',
   `categoria` int(11) NOT NULL,
-  `ficha_tecnica` int(11),
+  `ficha_tecnica` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_vehiculo`),
   KEY `FK_vehiculo_categoria` (`categoria`),
   KEY `FK_vehiculo_ficha_tecnica` (`ficha_tecnica`),
   CONSTRAINT `FK_vehiculo_categoria` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_vehiculo_ficha_tecnica` FOREIGN KEY (`ficha_tecnica`) REFERENCES `ficha_tecnica` (`id_ficha_tecnica`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Volcando datos para la tabla alquilercoches.vehiculos: ~0 rows (aproximadamente)
-DELETE FROM `vehiculos`;
+--
+-- Dumping data for table `vehiculos`
+--
+
+LOCK TABLES `vehiculos` WRITE;
 /*!40000 ALTER TABLE `vehiculos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `vehiculos` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-23 21:25:57
